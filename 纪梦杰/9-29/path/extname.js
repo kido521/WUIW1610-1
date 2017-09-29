0cf5d9e1ea6ceb323b0be24e221037f78675924d
satoshi1126 <291834072@qq.com>
2017-09-29 17:59:48 +0800

    练习

---
+++ 纪梦杰/9-29/path/extname.js
@@ -0,0 +1,5 @@
+var path = require('path');
+
+var p=__filename;
+// 后缀
+console.log(path.extname(p));
---
+++ 纪梦杰/9-29/path/normalize.js
@@ -0,0 +1,3 @@
+var path = require('path');
+var p1="E:/Php+mysql/./Nodejs/9.28";
+console.log(path.normalize(p1));
