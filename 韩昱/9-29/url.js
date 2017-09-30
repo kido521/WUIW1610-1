3904dbff347c4d4c1f5f44e73437b9c8c8b9bff6
Barryu <1293777259@qq.com>
2017-09-30 11:29:16 +0800

    url,path

---
+++ 韩昱/9-29/url.js
@@ -0,0 +1,31 @@
+var url =require("url")
+// console.log(url)
+// var u="https://hao.360.cn/?src=lm&ls=n57cc1a139a";
+// var result=url.parse(u);
+// console.log(result)
+
+
+//     .......................................................
+
+
+// var a = url.format({
+//     protocol : 'http' ,
+//     auth : null ,
+//     host : 'baidu.com:8080' ,
+//     port : '8080' ,
+//     hostname : 'example.com' ,
+//     hash : null ,
+//     search : '?a=index&t=article&m=default',
+//     query : 'a=index&t=article&m=default',
+//     pathname : '/one',
+//     path : '/one?a=index&t=article&m=default',
+//     href : 'http://example.com:8080/one?a=index&t=article&m=default'
+// });
+// console.log(url.format(a))
+
+
+
+// console.log(url.resolve('/one/个人文件/three', 'four'));// '/one/two/four'
+// url.resolve('/one/two/three', 'four');         // '/one/two/four'
+// url.resolve('http://example.com/', '/one');    // 'http://example.com/one'
+// url.resolve('http://example.com/one', '/two'); // 'http://example.com/two'
