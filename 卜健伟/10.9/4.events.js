290a79d8dbc5a33b73d1c7f7663b3b876126ccbe
Jianwei <Jianwei>
2017-10-09 12:17:48 +0800

    huhu

---
+++ 卜健伟/10.9/4.events.js
@@ -0,0 +1,19 @@
+var emitter=require("events");
+var util=require("util");
+function person(name,age){
+    this.name=name;
+    this.age=age;
+}
+util.inherits(person,emitter);
+var xh=new person('小哥',18);
+function aa(){
+    console.log(this.name);
+}
+function bb(){
+    console.log(this.age);
+}
+xh.on('say',aa);
+xh.on('say',bb);
+xh.emit('say');
+xh.removeListener('say',aa);
+xh.emit('say');
