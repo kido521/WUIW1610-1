821f5935212f68ee32e882b6f508a0dc967b60e6
mrwangyongsheng <1733711794@qq.com>
2017-10-10 11:27:53 +0800

    req 和 ajax

---
+++ 王永胜/10-10/10.10ajax/ajax.js
@@ -0,0 +1,29 @@
+var http = require("http")
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var data = ['zhangsan','lisi','wangmazi','zhangsan'];
+http.createServer(function (req,res) {
+    var u = url.parse(req.url);
+    if(u.pathname =="/favicon.ico"){
+        res.end();
+        return;
+    }
+    if(u.pathname == "/"){
+        fs.readFile('./index.html',function(err,data){
+            res.write(data);
+            res.end();
+        })
+        return;
+    }
+    if(u.pathname=='/search'){
+        var q = qs.parse(u.query);
+        var result = data.filter(function (v,i) {
+            if(v == q.s){
+                return true;
+            }
+            return false;
+        })
+        res.end(JSON.stringify(result));
+    }
+}).listen(9000);
